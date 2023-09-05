09be0215757e35c1880e1293571714e470942b24
Lily Haverlag <flannyha@gmail.com>
2023-09-05 15:26:04 +0200

    Create window, device, metal layer, and NS view

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use metal_rs::MTLPixelFormat;
+
 fn main() {
-    println!("Hello, world!");
+    // Create a window
+    let event_loop = winit::event_loop::EventLoop::new();
+    let res = winit::dpi::LogicalSize::new(1280, 720);
+    let window = winit::window::WindowBuilder::new()
+        .with_inner_size(res)
+        .with_title("RustRenderMetal".to_string())
+        .build(&event_loop)
+        .unwrap();
+
+    // Create device
+    let device = Device::system_default().expect("Could not create device.");
+
+    // Create metal layer
+    let layer = MetalLayer::new();
+    layer.set_device(&device);
+    layer.set_pixel_format(MTLPixelFormat::RGBA8Unorm);
+    layer.set_presents_with_transaction(false);
+
+    // Create view - a sort of canvas where you draw graphics using Metal commands
+    unsafe {
+        let view = window.ns_view() as cocoa_id;
+        view.setWantsLayer(YES);
+        view.setLayer(mem::transmute(layer.as_ref()));
+    }
 }
