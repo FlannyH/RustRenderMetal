9e3125603a95e9ce5ebe1f049e3bb926ea14d36a
Lily Haverlag <flannyha@gmail.com>
2023-09-06 12:15:50 +0200

    Hello Window (red edition)

--- metal/shaders/hello_triangle.metal
+++ metal/shaders/hello_triangle.metal
@@ -14,10 +14,24 @@ struct VertexShaderOutput {
     float4 color;
 };
 
+struct Rect {
+    float x;
+    float y;
+    float w;
+    float h;
+};
+
+struct Color {
+    float r;
+    float g;
+    float b;
+    float a;
+};
+
 // Vertex shader function
 vertex VertexShaderOutput hello_triangle_vertex(const device vertex_t* vertex_array [[buffer(0)]], uint vertex_index [[vertex_id]]) {
     VertexShaderOutput out;
-    vertex_t vtx = vertex_array[vertex_index];
+    const device vertex_t& vtx = vertex_array[vertex_index];
     out.color = float4(vtx.color.r, vtx.color.g, vtx.color.b, 1.0);
     out.position = float4(vtx.position.x, vtx.position.y, 0.0, 1.0);
     return out;
@@ -27,3 +41,53 @@ vertex VertexShaderOutput hello_triangle_vertex(const device vertex_t* vertex_ar
 fragment float4 hello_triangle_fragment(VertexShaderOutput in [[stage_in]]) {
     return in.color;
 }
+struct ClearRect {
+    Rect rect;
+    Color color;
+};
+
+float2 rect_vert(
+    Rect rect,
+    uint vid
+) {
+    float2 pos;
+
+    float left = rect.x;
+    float right = rect.x + rect.w;
+    float bottom = rect.y;
+    float top = rect.y + rect.h;
+
+    switch (vid) {
+    case 0:
+        pos = float2(right, top);
+        break;
+    case 1:
+        pos = float2(left, top);
+        break;
+    case 2:
+        pos = float2(right, bottom);
+        break;
+    case 3:
+        pos = float2(left, bottom);
+        break;
+    }
+    return pos;
+}
+
+vertex VertexShaderOutput clear_rect_vertex(
+    const device ClearRect *clear_rect [[ buffer(0) ]],
+    unsigned int vid [[ vertex_id ]]
+) {
+    VertexShaderOutput out;
+    float4 pos = float4(rect_vert(clear_rect->rect, vid), 0, 1);
+    auto col = clear_rect->color;
+
+    out.position = pos;
+    out.color = float4(col.r, col.g, col.b, col.a);
+    return out;
+}
+
+fragment float4 clear_rect_fragment(VertexShaderOutput in [[stage_in]])
+{
+    return in.color;
+};
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,50 @@
-use std::fs::File;
+use std::{fs::File, mem};
 
-use metal_rs::{MTLPixelFormat, CompileOptions};
+use cocoa::base::YES;
+use cocoa::{appkit::NSView, base::id as cocoa_id};
+use glam::{Vec2, Vec3, Vec4};
+use metal::objc::rc::autoreleasepool;
+use metal::{Device, MetalLayer, MTLPixelFormat, LibraryRef, DeviceRef, RenderPipelineState, RenderPipelineDescriptor, MTLResourceOptions, MTLLoadAction, RenderPassDescriptor, MTLClearColor, MTLStoreAction, MTLScissorRect, MTLPrimitiveType};
+use winit::event::{Event, WindowEvent};
+use winit::event_loop::ControlFlow;
+use winit::platform::macos::WindowExtMacOS;
+use core_graphics_types::geometry::CGSize;
 
+
+#[repr(C)]
+struct HelloTriangleVertex {
+    position: Vec2,
+    color: Vec3,
+}
+
+#[repr(C)]
+struct ClearRect {
+    pub rect: Vec4,
+    pub color: Vec4,
+}
+
+fn prepare_pipeline_state (
+    device: &DeviceRef,
+    library: &LibraryRef,
+    vertex_shader_path: &str,
+    fragment_shader_path: &str,
+ ) -> RenderPipelineState {
+    // Get compiled functions from the library
+    let vertex_function = library.get_function(vertex_shader_path, None).unwrap();
+    let fragment_function = library.get_function(fragment_shader_path, None).unwrap();
+
+    // Create pipeline state descriptor - handles things like shader program, buffer to render to, blend mode, etc.
+    let pipeline_state_desc = RenderPipelineDescriptor::new();
+    pipeline_state_desc.set_vertex_function(Some(&vertex_function));
+    pipeline_state_desc.set_fragment_function(Some(&fragment_function));
+
+    let attachment = pipeline_state_desc.color_attachments().object_at(0).unwrap();
+    attachment.set_pixel_format(MTLPixelFormat::RGBA8Unorm);
+    attachment.set_blending_enabled(false);
+    return device.new_render_pipeline_state(&pipeline_state_desc).unwrap();
+}
+
+// Credits to https://github.com/gfx-rs/metal-rs/blob/master/examples/window/main.rs for the base structure
 fn main() {
     // Create a window
     let event_loop = winit::event_loop::EventLoop::new();
@@ -14,11 +57,6 @@ fn main() {
 
     // Create device
     let device = Device::system_default().expect("Could not create device.");
-
-    // Load shader source
-    let mut shader_source_file = File::open("metal/shaders/hello_triangle.metal").expect("Failed to open shader file.");
-    let mut source = String::new();
-    file.read_to_string(&mut source).expect("Failed to read file.");
 
     // Create metal layer
     let layer = MetalLayer::new();
@@ -33,5 +71,92 @@ fn main() {
         view.setLayer(mem::transmute(layer.as_ref()));
     }
 
+    let drawable_size = window.inner_size();
+    layer.set_drawable_size(CGSize::new(drawable_size.width as f64, drawable_size.height as f64));
 
+    // Load the Metal library file
+    let library = device.new_library_with_file("metal/shaders/hello_triangle.metallib").expect("Failed to load Metal library");
+
+    // Initialize the pipeline states with functions from this library
+    let hello_triangle_pipeline_state = prepare_pipeline_state(&device, &library, "hello_triangle_vertex", "hello_triangle_fragment");
+    let clear_rect_pipeline_state = prepare_pipeline_state(&device, &library, "clear_rect_vertex", "clear_rect_fragment");
+
+    // Create command queue
+    let command_queue = device.new_command_queue();
+
+    // Set up vertex buffer data for the triangle
+    let mut vertex_buffer_data = Vec::<HelloTriangleVertex>::new();
+    // todo: make sure the winding order is correct
+    vertex_buffer_data.push(HelloTriangleVertex{ position: Vec2{x: -1.0, y: 1.0}, color: Vec3{x: 1.0, y: 0.0, z: 0.0} });
+    vertex_buffer_data.push(HelloTriangleVertex{ position: Vec2{x: 1.0, y: 1.0}, color: Vec3{x: 0.0, y: 1.0, z: 0.0} });
+    vertex_buffer_data.push(HelloTriangleVertex{ position: Vec2{x: 0.0, y: 0.0}, color: Vec3{x: 0.0, y: 0.0, z: 1.0} });
+
+    // Create the vertex buffer on the device
+    let vertex_buffer = device.new_buffer_with_data(
+        vertex_buffer_data.as_ptr() as *const _,
+        (vertex_buffer_data.len() * mem::size_of::<HelloTriangleVertex>()) as u64,
+        MTLResourceOptions::CPUCacheModeDefaultCache | MTLResourceOptions::StorageModeManaged,
+    );
+
+    // Create a full screen rectangle to use for clearing the screen
+    let clear_rect = vec![ClearRect {
+        rect: Vec4 {x: -1.0, y: -1.0, z: 2.0, w: 2.0},
+        color: Vec4 {x: 0.5, y: 0.8, z: 0.5, w: 1.0},
+    }];
+
+    let clear_rect_buffer = device.new_buffer_with_data(
+        clear_rect.as_ptr() as *const _,
+        mem::size_of::<ClearRect>() as u64,
+        MTLResourceOptions::CPUCacheModeDefaultCache | MTLResourceOptions::StorageModeManaged,
+    );
+
+    // Main loop
+    event_loop.run(move |event, _, control_flow| {
+        autoreleasepool(|| {
+            *control_flow = ControlFlow::Poll;
+
+            println!("{:?}", event);
+            match event {
+                Event::WindowEvent{event, ..} => match event {
+                    WindowEvent::CloseRequested => *control_flow = ControlFlow::Exit,
+                    WindowEvent::Resized(size) => layer.set_drawable_size(CGSize::new(size.width as f64, size.height as f64)),
+                    _ => (),
+                }
+                Event::MainEventsCleared => {
+                    window.request_redraw();
+                }
+                Event::RedrawRequested(_) => {
+                    // Get the next framebuffer
+                    let drawable = match layer.next_drawable() {
+                        Some(drawable) => drawable,
+                        None => return,
+                    };
+
+                    // Set up framebuffer
+                    let render_pass_descriptor = RenderPassDescriptor::new();
+                    let color_attachment = render_pass_descriptor.color_attachments().object_at(0).unwrap();
+                    color_attachment.set_texture(Some(drawable.texture()));
+                    color_attachment.set_load_action(MTLLoadAction::Clear);
+                    color_attachment.set_clear_color(MTLClearColor::new(1.0, 0.2, 0.25, 1.0));
+                    color_attachment.set_store_action(MTLStoreAction::Store);
+                    
+                    // Set up command buffer
+                    let command_buffer = command_queue.new_command_buffer();
+                    let command_encoder = command_buffer.new_render_command_encoder(&render_pass_descriptor);
+
+                    // Record triangle draw call
+                    println!("{}", vertex_buffer_data.len());
+                    command_encoder.set_scissor_rect(MTLScissorRect{x: 0, y: 0, width: drawable_size.width as u64, height: drawable_size.height as u64});
+                    command_encoder.set_vertex_buffer(0, Some(&vertex_buffer), 0);
+                    //command_encoder.draw_primitives(MTLPrimitiveType::Triangle, 0, vertex_buffer_data.len() as u64);
+                    command_encoder.end_encoding();
+
+                    // Present framebuffer
+                    command_buffer.present_drawable(&drawable);
+                    command_buffer.commit();
+                }
+                _ => {}
+            }
+        });
+    });
 }
